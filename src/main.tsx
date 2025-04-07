d68fc2cff20153913e17e56215879d6ff851f1dd
MinuraWije <minurawije@gmail.com>
2025-04-07 10:54:10 +0530

    updated import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {Provider} from "react-redux";
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from "react-router-dom";
 import store from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
