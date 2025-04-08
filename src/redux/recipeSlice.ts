6099ea23353bc089eee27e87590bbe791157d2dd
MinuraWije <minurawije@gmail.com>
2025-04-08 11:06:54 +0530

    created recipeSlice

---
+++ src/redux/recipeSlice.ts
@@ -0,0 +1,49 @@
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit"
+import axios from "axios"
+import {Recipe} from "../types";
+
+
+interface RecipeState{
+    recipes: Recipe[]
+    loading:boolean
+    error:string | null
+}
+
+const initialState:RecipeState = {
+    recipes: [],
+    loading: false,
+    error: null,
+}
+
+export const fetchRecipes = createAsyncThunk('recipes/fetch', async () => {
+    const res = await axios.get('/api/recipes')
+    return res.data;
+})
+
+const recipeSlice = createSlice({
+    name: 'recipes',
+    initialState,
+    reducers:{
+        addRecipe: (state, action:PayloadAction<Recipe>) => {
+            state.recipes.push(action.payload)
+        },
+    },
+
+    extraReducers:(builder) => {
+        builder
+            .addCase(fetchRecipes.pending, (state, action) => {
+                state.loading = true
+            })
+            .addCase(fetchRecipes.fulfilled, (state, action) => {
+                state.loading = false
+                state.recipes = action.payload
+            })
+            .addCase(fetchRecipes.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.error.message || 'Failed to fetch recipes'
+            })
+    }
+})
+
+export const {addRecipe} = recipeSlice.actions;
+export default recipeSlice.reducer
