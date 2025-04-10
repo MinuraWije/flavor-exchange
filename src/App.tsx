147934e711701ddd731b0e69e0053a5d36b250e0
MinuraWije <minurawije@gmail.com>
2025-04-10 21:42:33 +0530

    removed unnecessary routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,37 @@ import { RootState } from './redux/store'
 import { useSelector } from 'react-redux'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import Navbar from "./components/Navbar.tsx";
+import Layout from "./components/Layout.tsx";
+import RecipePage from './pages/RecipePage.tsx'
+import FavoritesPage from "./pages/FavoritesPage.tsx";
+import MyRecipesPage from "./pages/MyRecipesPage.tsx";
 
 function App() {
     const user = useSelector((state: RootState) => state.auth.user)
     return (
         <>
-            <Navbar/>
+
             <div className="p-4">
                 <Routes>
-                    <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-                    <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
+                    <Route path="/" element={<Navigate to={user ? "/home" : "/login"} />} />
 
-                    {/*<Route path="/" element={<Home />}/>
-                <Route path="/recipe/:id" element={<RecipeDetails />}/>
-                <Route path="/add" element={<AddRecipe />}/>
-                <Route path="/login" element={<Login />}/>
-                <Route path="/signup" element={<Signup />}/>*/}
+                    <Route path="/login" element={!user ? <Login /> : <Navigate to="/home" />} />
+                    <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/home" />} />
+
+                    {user && (
+                        <Route element={<Layout />}>
+                            <Route path="/home" element={<Home />} />
+
+                            <Route path="/favorites" element={<FavoritesPage />} />
+
+                            <Route path="/my-recipes" element={<MyRecipesPage />} />
+
+
+                            <Route path="/recipe/:id" element={<RecipePage />} />
+
+
+                        </Route>
+                    )}
                 </Routes>
             </div>
         </>
@@ -30,12 +44,3 @@ function App() {
 
 export default App
 
-/*const App = () => {
-    return (
-        <div className="p-10 bg-red-500 text-white text-2xl font-bold">
-            🚀 Tailwind is working!
-        </div>
-    );
-};
-
-export default App;*/
