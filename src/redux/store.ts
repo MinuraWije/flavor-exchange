fea055f14b91aa0d1b86a621fc1eb804d29dca54
MinuraWije <minurawije@gmail.com>
2025-04-10 15:46:24 +0530

    added favorites slice to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './authSlice.ts'
 import recipeReducer from './recipeSlice.ts'
+import favoritesReducer from './favoritesSlice.ts'
 
 const store = configureStore({
     reducer: {
         auth:authReducer,
         recipes:recipeReducer,
+        favorites:favoritesReducer,
     },
 });
 
